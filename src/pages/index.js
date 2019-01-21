7a71e7f0fb6dba520538ba7bb7b051cf99ab3adb
bdmllc <beachdaymedia@gmail.com>
2019-01-21 14:21:06 -0600

    another commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <h3>Hello from Gatsby</h3>
-      <div className="container">
-        <div className="row">
-          <div className="col-6">Hello World</div>
-          <div className="col-6">Hola</div>
-        </div>
-      </div>
+      
   </Layout>
 )
 
