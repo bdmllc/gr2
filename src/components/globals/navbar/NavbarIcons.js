40be6580dde78c9b0050f7753eef868cb40fb1c3
bdmllc <beachdaymedia@gmail.com>
2019-01-22 19:44:07 -0600

    another commit

--- src/components/globals/navbar/NavbarIcons.js
+++ src/components/globals/navbar/NavbarIcons.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
+import styled from 'styled-components';
+import { styles } from '../../../utils';
+import { FaInstagram, FaTwitter, FaFacebook } from 'react-icons/fa'
+
+//Video # 19 paused at 10:42
 
 export default class NavbarIcons extends Component {
+  state = {
+    icons: [
+      { 
+        id: 1, 
+        icon: <FaFacebook className="icon facebook-icon" />,
+        path: 'https://www.facebook.com'
+      }, 
+      { 
+        id: 2, 
+        icon: <FaTwitter className="icon twitter-icon" />,
+        path: 'https://www.twitter.com'
+      }, 
+      { 
+        id: 3, 
+        icon: <FaInstagram className="icon instagram-icon" />,
+        path: 'https://www.instagram.com'
+      }, 
+    ],
+  }
   render() {
     return (
-      <div>
-        content from navbar icons
-      </div>
+      <IconWrapper>
+        {this.state.icons.map(item => {
+          return <a href={item.path} key={item.id} target="_blank" rel="noopener noreferrer">{item.icon}</a>
+        })}
+      </IconWrapper>
     )
   }
 }
+
+const IconWrapper = styled.div`
+.icon {
+  font-size: 1.3rem;
+  cursor: pointer;
+}
+.icon:hover{
+
+}
+`;
