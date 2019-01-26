eb9f846362478950d2a2e3aa32d0902518c300e4
bdmllc <beachdaymedia@gmail.com>
2019-01-26 10:34:47 -0600

    another commit

---
+++ src/components/HomePageComponents/Gallery.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Gallery() {
+  return (
+    <div>
+      <h1>Hello from the gallery section</h1>
+    </div>
+  )
+}
--- src/components/image.js
+++ src/components/image.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+// import React from 'react'
+// import { StaticQuery, graphql } from 'gatsby'
+// import Img from 'gatsby-image'
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -17,7 +17,7 @@ import Img from 'gatsby-image'
 //   <StaticQuery
 //     query={graphql`
 //       query {
-//         placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+//         placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" })           {
 //           childImageSharp {
 //             fluid(maxWidth: 300) {
 //               ...GatsbyImageSharpFluid
@@ -30,3 +30,4 @@ import Img from 'gatsby-image'
 //   />
 // )
 // export default Image
+
